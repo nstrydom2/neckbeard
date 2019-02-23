a92ed788bf0259a118bd4d80b0a16f15665379b3
Nick <nstrydom@gmail.com>
2019-02-22 19:40:19 -0600

    Modified 'generate_meme.gif' to be more intuitive

--- neckbeard.py
+++ neckbeard.py
@@ -2,41 +2,39 @@ from moviepy.editor import *
 
 class NeckBeard():
     def __init__(self):
-        self.trump_vid = '/home/ghost/Downloads/trumps-most-awkward-moments-of-2018.mp4'
+        pass
 
-    def generate_meme(self, pic_path):
-        with open(pic_path) as in_file:
-            pass
-
-    # Generate a gif
+    # Generate a gif meme with text on the top
     # parameters:
     #   start_time/end_time format = 4,11.4
     #   text_postion = tuple ex) ('center', 'bottom')
-    def generate_gif(self, start_time, end_time, text=None, text_position=None):
-        clip = (VideoFileClip(self.trump_vid).subclip((start_time), (4,11.4)).resize(0.45))
+    def generate_gif(self, vid_path, start_time=None, end_time=None, out='./NEW_MEME.gif', resize=1.0,
+                     text=None, text_position=None, text_size=45.0, text_font='Impact', text_color='white'):
+        clip = None
+        composition = None
 
-        text = (TextClip('I\'m just going to leave this here', fontsize=45.0, font='Impact', color='white')
-            .set_position(('center', 'bottom'))
-            .set_duration(clip.duration))
+        if start_time is not None and \
+            end_time is not None:
+            clip = (VideoFileClip(vid_path).subclip(start_time, end_time).resize(resize))
+        else:
+            clip = (VideoFileClip(vid_path).resize(resize))
 
-        composition = CompositeVideoClip([clip, text])
-        composition.write_gif('leave_this_here.gif')
+        if text is not None:
+            caption = (TextClip(text, fontsize=text_size, font=text_font, color=text_color)
+                .set_position(text_position)
+                .set_duration(clip.duration))
+            composition = CompositeVideoClip([clip, caption])
+        else:
+            composition = CompositeVideoClip([clip])
 
-    def generate_baby_gif(self):
-        vid = '/home/ghost/Downloads/reddit-cringe-compilation-most-awkward-moments-2019.mp4'
-
-        clip = (VideoFileClip(vid).subclip((4,10.6), (4,13.4)).resize(0.3))
-
-        text = (TextClip('The Priest really takes his job seriously'.upper(), fontsize=18.0, font='Impact', color='white')
-            .set_position(('center', 'top'))
-            .set_duration(clip.duration))
-
-        composition = CompositeVideoClip([clip, text])
-        composition.write_gif('priest_small.gif')
-
+        composition.write_gif(out)
 
 if __name__ == '__main__':
     gif_maker = NeckBeard()
 
-    gif_maker.generate_baby_gif()
+    gif_maker.generate_gif('/home/ghost/Downloads/luke.gif',
+                           out='./funny-meme.gif',
+                           resize=0.6,
+                           text='NOOOOOOO',
+                           text_position=('center', 'bottom'))
  
